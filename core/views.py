197b40bdf27c850ad81e24fc6f94c5a7c4469dd9
Niramay Thaker <thakerniramay@gmail.com>
2023-02-18 18:42:34 +0530

    Setting up base and login signup functionality

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello Django ..!")
+	return render(request, 'core/index.html')
--- get_set_work/settings.py
+++ get_set_work/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "core.apps.CoreConfig",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -55,8 +62,9 @@ ROOT_URLCONF = "get_set_work.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-		,
+        "DIRS": [
+            BASE_DIR / 'core/templates'
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -112,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
