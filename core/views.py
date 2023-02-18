741b47b0c81ec95b27cbd8c33a4d17d19e344f5b
Niramay Thaker <thakerniramay@gmail.com>
2023-02-18 18:55:22 +0530

    Login signup functionality

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .forms import RegistrationForm
 
 
 # Create your views here.
 def index(request):
 	return render(request, 'core/index.html')
+
+
+def sign_up(request):
+	form = RegistrationForm()
+	if request.method == 'POST':
+		pass
+
+	context = {'form': form}
+	return render(request, 'registration/signup.html', context=context)
